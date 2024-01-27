f10c7ac59dcf8b8b2bf2c03c01dfdabae28d6ce4
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-01-27 15:44:22 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+export default function App() {
+  return (
+    <Box component='div'>
+      Hello World!
+    </Box>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />}></Route>
+    </Routes>
+  </BrowserRouter>
+)

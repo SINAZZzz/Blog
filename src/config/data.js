ba3f5758010ef16dc50b1ed2a5d708fa93afd7a1
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-03-14 05:18:55 +0330

    clear data js

--- src/config/data.js
+++ src/config/data.js
@@ -2,57 +2,6 @@ import { faker } from "@faker-js/faker";
 
 const items = 10;
 
-// export const blogList = [
-//   {
-//     id: faker.number.int(),
-//     title: faker.company.name(),
-//     category: faker.commerce.product(),
-//     subCategory: [
-//       faker.lorem.word(),
-//       faker.lorem.word(),
-//       faker.lorem.word(),
-//       faker.lorem.word(),
-//     ],
-//     description: faker.lorem.paragraph(),
-//     authorName: faker.person.fullName(),
-//     authorAvatar: faker.image.avatar(),
-//     createdAt: faker.date.weekday() + " " + faker.date.month(),
-//     cover: faker.image.url(),
-//   },
-//   {
-//     id: faker.number.int(),
-//     title: faker.company.name(),
-//     category: faker.commerce.product(),
-//     subCategory: [
-//       faker.lorem.word(),
-//       faker.lorem.word(),
-//       faker.lorem.word(),
-//       faker.lorem.word(),
-//     ],
-//     description: faker.lorem.paragraph(),
-//     authorName: faker.person.fullName(),
-//     authorAvatar: faker.image.avatar(),
-//     createdAt: faker.date.weekday() + " " + faker.date.month(),
-//     cover: faker.image.url(),
-//   },
-//   {
-//     id: faker.number.int(),
-//     title: faker.company.name(),
-//     category: faker.commerce.product(),
-//     subCategory: [
-//       faker.lorem.word(),
-//       faker.lorem.word(),
-//       faker.lorem.word(),
-//       faker.lorem.word(),
-//     ],
-//     description: faker.lorem.paragraph(),
-//     authorName: faker.person.fullName(),
-//     authorAvatar: faker.image.avatar(),
-//     createdAt: faker.date.weekday() + " " + faker.date.month(),
-//     cover: faker.image.url(),
-//   },
-// ];
-
 export const blogList = Array.from({ length: items }, () => ({
   id: faker.number.int(),
   title: faker.company.name(),

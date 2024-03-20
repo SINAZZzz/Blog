d194280b0f07b8296b747389b196e973b948a4be
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-03-20 14:36:24 +0330

    new update

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -6,19 +6,16 @@ export default function Profile() {
   const { id } = useContext(AppContext);
   console.log(id);
 
-  // Finding the profile with the matching id
   const profile = blogList.find((profile) => profile.id === id);
 
-  // Handling the case when the profile is not found
   if (!profile) {
     return <div>No data found for this ID.</div>;
   }
 
-  // Rendering the profile data
   return (
     <div>
-      <p>{profile.authorName}</p>
-      {/* Render other profile data here */}
+      <img src={profile.authorAvatar} alt="pic user" />
+      <p> name: {profile.authorName}</p>
     </div>
   );
 }

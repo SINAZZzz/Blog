9b27beb7415520aaf6f3375db3fe2026e2fee5bc
Sina Zinsaz <sinazinsaz1@gamil.com>
2024-03-20 16:50:13 +0330

    new add

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,6 +1,9 @@
 import React, { useContext } from "react";
 import { blogList } from "../../config/data.js";
 import { AppContext } from "../../context/AppContext.jsx";
+import "./style.css";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
 
 export default function Profile() {
   const { id } = useContext(AppContext);
@@ -14,8 +17,45 @@ export default function Profile() {
 
   return (
     <div>
-      <img src={profile.authorAvatar} alt="pic user" />
-      <p> name: {profile.authorName}</p>
+      <Link className="blog-goBack" to="/">
+        <span> &#8592;</span> <span>Go Back</span>
+      </Link>
+      <div className="profile">
+        <img
+          src={profile.authorAvatar}
+          alt="pic user"
+          className="authorAvatar"
+        />
+        <div>
+          <p className="username">
+            {" "}
+            <span>name :</span> {profile.authorName}
+          </p>
+        </div>
+      </div>
+      <div className="card_post">
+        <h1>Posts</h1>
+        <Box component="div" className="card">
+          <Link className="link" to={`/blog/${profile.id}`}>
+            <Box
+              component="img"
+              className="cover"
+              src={profile.cover}
+              alt="cover"
+            />
+          </Link>
+          <Box component="p" className="chip">
+            {profile.category}
+          </Box>
+          <Link className="link" to={`/blog/${profile.id}`}>
+            <Box component="h3">{profile.title}</Box>
+          </Link>
+
+          <Box component="p" className="description">
+            {profile.description}
+          </Box>
+        </Box>
+      </div>
     </div>
   );
 }
